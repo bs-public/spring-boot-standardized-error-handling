3a8b5b1889429f3800de775df999073338c9c098
bs <26665808+bs-public@users.noreply.github.com>
2025-06-07 16:11:34 +1000

    Add User entity and repository

---
+++ src/main/java/com/app/entity/User.java
@@ -0,0 +1,34 @@
+package com.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank(message = "Name is required")
+  private String name;
+
+  @Email(message = "Email should be valid")
+  @Column(unique = true)
+  private String email;
+}
